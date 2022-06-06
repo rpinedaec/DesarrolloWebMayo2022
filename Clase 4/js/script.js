420fbb871ab1bcf4f7cf1af510931fb12970d0ac
sebjosa7 <seb.josa7@gmail.com>
2022-06-05 19:35:43 -0500

    o

--- Clase 4/js/script.js
+++ Clase 4/js/script.js
@@ -1,6 +1,9 @@
 let jugar = document.getElementById("jugar");
 let intentos = 0;
 let totalPuntaje = 0;
+$("#resetear").on( "click", function() {
+    location.reload();
+  });
 
 jugar.addEventListener("click",function(){
     console.log(intentos);
@@ -9,23 +12,32 @@ jugar.addEventListener("click",function(){
         $("#exampleModal").modal('show');
         let resultado = getRandomInt(1,6);
         console.log(resultado);
-
         setTimeout(() => {
-            
+           
             intentos ++;
             $("#dado"+intentos).attr('src', "img/"+resultado+".jpg");
-            $("#intento" + intentos).text(intentos);
-            $("#valor" + intentos).text(resultado);
-            let puntaje = document.getElementById("puntaje"+intentos);
+            $("#intento"+ intentos).text(intentos);
+            $("#valor"+ intentos).text(resultado);
+            let puntaje =document.getElementById("puntaje"+intentos);
             puntaje.innerText= resultado;
-            totalPuntaje = totalPuntaje + resultado;
+            totalPuntaje = totalPuntaje+ resultado;
             let total = document.getElementById("total");
             total.innerText = totalPuntaje;
-            
+           
             $("#exampleModal").modal('hide');
+            $("#container").show();
         }, 2000);
     }
     else{
-        alert("intentos completos");
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Intentos Completos'
+          })
     }
+
 });
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
