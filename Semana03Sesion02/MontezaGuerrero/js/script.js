7df09419d1e36c44006cab94297e5d45cb6b2ffa
JosephMG <jodsmg023@gmail.com>
2022-06-12 19:01:32 -0500

    Aplicando cambios

--- Semana03Sesion02/MontezaGuerrero/js/script.js
+++ Semana03Sesion02/MontezaGuerrero/js/script.js
@@ -4,7 +4,7 @@ var $table = $('#tblResultados');
 let usuariosAgregados = [];
 
 $("#buscar").on("click", function () {
-    let url = "https://randomuser.me/api/?results=50";
+    let url = "https://randomuser.me/api/?results=10";
     fetch(url)
         .then((response) => response.json())
         .then(
@@ -64,21 +64,17 @@ $('#opciones').on('change', function () {
 
 function operateFormatter(value, row, index) {
     return [
-        '<a class="like" href="javascript:void(0)" title="Like">',
-        'Agregar',
+        '<a class="like btn btn-primary" href="javascript:void(0)" title="Like">',
+        'Agregar <i class="fa-solid fa-plus"></i>',
         '</a>  ',
-        '<a class="remove" href="javascript:void(0)" title="Remove">',
-        'Eliminar',
+        '<a class="remove btn btn-danger" href="javascript:void(0)" title="Remove">',
+        '<i class="fa-solid fa-trash-can"></i>' ,
         '</a>'
     ].join('')
 }
 window.operateEvents = {
     'click .like': function (e, value, row, index) {
         usuariosAgregados.push(row);
-        console.log(e);
-        console.log(value);
-        console.log(row);
-        console.log(index);
 
     },
     'click .remove': function (e, value, row, index) {
@@ -93,9 +89,34 @@ function removeItemFromArr(arr, item) {
 
 $("#verDatos").on("click", function () {
     console.log(usuariosAgregados)
+    let html = '';
     usuariosAgregados.forEach(element => {
-        let html = '<div class="col-xs-12 col-sm-6 col-md-4"> <div class="image-flip" > <div class="mainflip flip-0"> <div class="frontside"> <div class="card"> <div class="card-body text-center"> <p><img class=" img-fluid" src="' + element.picture.large + ' " alt="card image"></p> <h4 class="card-title">' + element.name.title + " " + element.name.first + " " + element.name.last + '</h4> <p class="card-text">' + element.email + '</p> <a href="https://www.fiverr.com/share/qb8D02" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a> </div> </div> </div> <div class="backside"> <div class="card"> <div class="card-body text-center mt-4"> <h4 class="card-title">' + element.gender + ' </h4> <p class="card-text">' + element.cell + "<br>" + element.nat + ' </p> <ul class="list-inline"> <li class="list-inline-item"> <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02"> <i class="fa fa-facebook"></i> </a> </li> <li class="list-inline-item"> <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02"> <i class="fa fa-twitter"></i> </a> </li> <li class="list-inline-item"> <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02"> <i class="fa fa-skype"></i> </a> </li> <li class="list-inline-item"> <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02"> <i class="fa fa-google"></i> </a> </li> </ul> </div> </div> </div> </div> </div> </div>';
-        $("#teams").append(html);
+        html += 
+        `
+        <div class="col-xs-12 col-sm-6 col-md-4 mt-3"> 
+            <div class=`
+            element.gender == "male" ? html += '"classMale"' : html += '"classFemale"';
+        html +=             `>
+                <div class="card">
+                    <div class="card-body">
+                        <img class= `
+                        element.gender == "male" ? html += '"classMale"' : html += '"classFemale "';
+                        
+        html +=         `src="${element.picture.large}" alt="Card image" style="width:100%">
+                        <h4 class="card-title mt-3 d-flex justify-content-center">${element.name.title} ${element.name.first} ${element.name.last}</h4>
+                        <p class="card-text mt-3 d-flex justify-content-center">${element.email}</p>
+                        <div class="redesSociales">
+                            <i class="fa-brands fa-facebook"></i>
+                            <i class="fa-brands fa-twitter"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div> 
+        `;
+        $("#teams").html(html);
     });
 });
 
